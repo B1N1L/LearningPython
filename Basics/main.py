5825aaceaf0b8a6023671f8eb8ee9790b2273200
B1N1L <binilbabu1997@gmail.com>
2023-11-12 07:00:34 +0530

    Learned about functions

--- Basics/main.py
+++ Basics/main.py
@@ -1,19 +1,41 @@
-print("Hello World")
+#print("Hello World")
 
 # Simple Calculator Program
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-operator = input("Enter the operator (+,-,*,/): ")
-if(operator == '+'):
-    output = num1+num2
-elif(operator == '-'):
-    output = num1-num2
-elif(operator == '*'):
-    output = num1*num2
-elif(operator == '/'):
-    output = num1/num2
-else:
-    print("Invalid operator")
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# operator = input("Enter the operator (+,-,*,/): ")
+# if(operator == '+'):
+#     output = num1+num2
+# elif(operator == '-'):
+#     output = num1-num2
+# elif(operator == '*'):
+#     output = num1*num2
+# elif(operator == '/'):
+#     output = num1/num2
+# else:
+#     print("Invalid operator")
 
-print("Output:", output)
+# print(f"Output: {output}")
+
+# Tip Calculator using Function
+
+# def FoodTotal(foodAmount, tipPercentage):
+#     tipAmount=foodAmount*(tipPercentage/100)
+#     totalAmount=foodAmount+tipAmount
+#     return totalAmount
+
+# print("Total Amount: ", FoodTotal(100,10))
+
+# Find the greater number
+
+# def greaterNum(num1: int, num2: int) -> int:
+#     if(num1>num2):
+#         return num1
+#     elif(num2>num1):
+#         return num2
+#     else:
+#         return 0
+
+# print(greaterNum(12,12))
+
